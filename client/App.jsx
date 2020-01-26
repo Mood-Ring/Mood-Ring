876ab9593bdfb61bca37d532f6afd6e9d7793c20
chelseyeslehc <chelsey.fewer@gmail.com>
2020-01-26 13:35:34 -0500

    added mapStateToProps and mapDispatchToProps for main App component

--- client/App.jsx
+++ client/App.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import MainHeader from './components/MainHeader.jsx';
 import LandingPageBody from './components/LandingPageBody.jsx';
 import BottomLinks from './components/BottomLinks.jsx';
@@ -7,45 +7,53 @@ import Feeling from './components/HowAreYouFeeling.jsx';
 import * as actions from '../redux/actions.js';
 import { connect } from 'react-redux';
 
-const page1 = "UserFeed";
+const page1 = 'UserFeed';
 
-// const mapStateToProps = ({
-//     reduxState: {page},
-// }) => ({
-//     page
-// })
+const mapStateToProps = (reduxState) => {
+  //used to bring in the pieces of state that the components on this page will use
+  return {
+    page: reduxState.page
+  };
+};
 
-
-const mapDispatchToProps = dispatch => ({
-
-});
-
-class App extends Component{
-    constructor(props){
-        super(props);
+const mapDispatchToProps = (dispatch) => {
+  //used to bring in actions that will be dispatched within the components on this page.
+  //   syncMarkets: () => dispatch(actions.syncMarkets())
+  return {
+    changePage: (index) => {
+      dispatch(actions.changePage(index));
     }
+  };
+};
 
-    render(){
-        const display = [];
-        display.push(<MainHeader/>);
-        switch(page1){
-            case "Home": 
-            display.push(<LandingPageBody/>);
-            break;
-            case "Login": 
-            display.push(<LogIn/>);
-            break;
-            case "UserFeed":
-            display.push(<Feeling/>);
-            break;
-        }
-        display.push(<BottomLinks/>)
-        return(
-            <div>
-               {display}
-            </div>
-        ) 
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    //changePage(0) serves the homepage for the app
+    this.props.changePage(0);
+  }
+
+  render() {
+    const display = [];
+    display.push(<MainHeader />);
+    switch (this.props.page) {
+      //this.props.page loads the homepage
+      case 'Home':
+        display.push(<LandingPageBody />);
+        break;
+      case 'Login':
+        display.push(<LogIn />);
+        break;
+      case 'UserFeed':
+        display.push(<Feeling />);
+        break;
     }
+    display.push(<BottomLinks />);
+    return <div>{display}</div>;
+  }
 }
 
-export default App/*connect(mapStateToProps, mapDispatchToProps)(App)*/;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
