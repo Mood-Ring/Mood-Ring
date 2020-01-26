ca70ff14bf3fa94d7fc4bb57a8b264519c74d8f6
Josh Naso <joshnaso@Joshs-MacBook-Pro.local>
2020-01-25 19:05:50 -0500

    ReactAndReduxStart

--- redux/store.js
+++ redux/store.js
@@ -1,7 +1,8 @@
 import { createStore } from 'redux';
 import reducers from './reducers.js';
+import {changePage} from './actions.js'
 
 const store = createStore(reducers);
-
+store.dispatch(changePage(0));
 console.log('getState', store.getState());
 export default store;
