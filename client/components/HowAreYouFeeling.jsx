20077f0d1992d41db5c50db0ddf1cd15d224e7a6
Vinh C <53790130+Vchau511@users.noreply.github.com>
2020-01-27 11:14:13 -0500

    serverpost

--- client/components/HowAreYouFeeling.jsx
+++ client/components/HowAreYouFeeling.jsx
@@ -8,58 +8,67 @@
 
 import React from 'react';
 import { Component } from 'react';
-import styled  from 'styled-components';
+import styled from 'styled-components';
+import { connect } from 'react-redux';
 
-//The main body styling 
+//The main body styling
 const MainDiv = styled.div`
-    width: 100%;
-    height: 300px;
-    border: solid;
-    margin-top: 10px;
-    text-align: center;
+  width: 100%;
+  height: 300px;
+  border: solid;
+  margin-top: 10px;
+  text-align: center;
 `;
 
 const SelectStyle = styled.select`
-    height: 100px;
-    width: 200px;
-    font-size: 30px;
+  height: 100px;
+  width: 200px;
+  font-size: 30px;
 `;
 
 const SubmitBitton = styled.button`
-    margin-top: 20px;
-    text-decoration: none;
-    border-radius: 20px;
-    color: black;
-    font-size: 20px;
-    &:focus{
-        outline: none;
-    }
+  margin-top: 20px;
+  text-decoration: none;
+  border-radius: 20px;
+  color: black;
+  font-size: 20px;
+  &:focus {
+    outline: none;
+  }
 `;
 
-class Feeling extends Component{
-    constructor(props){
-        super(props);
-    }
+const mapStateToProps = (reduxState) => {
+  //used to bring in the pieces of state that the components on this page will use
+  return {
+    currentUser: reduxState.currentUser
+  };
+};
 
-    render(){
-        return(
-            <MainDiv>
-                <h1>How are you feeling today?</h1>
-                <SelectStyle>
-                    <option value = "Happy">Happy</option>
-                    <option value = "Sad">Sad</option>
-                    <option value = "Frustrated">Frustrated</option>
-                    <option value = "Tired">Tired</option>
-                    <option value = "Relaxed">Relaxed</option>
-                    <option value = "Tense">Tense</option>
-                    <option value = "Excited">Excited</option>
-                    <option value = "Distracted">Distracted</option>
-                </SelectStyle>
-                <br></br>
-                <SubmitBitton>Submit</SubmitBitton>
-            </MainDiv>
-        )
-    }
+class Feeling extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <MainDiv>
+        <h1>How are you feeling today {this.props.currentUser}?</h1>
+        <SelectStyle>
+          <option value="Happy">Happy</option>
+          <option value="Sad">Sad</option>
+          <option value="Frustrated">Frustrated</option>
+          <option value="Tired">Tired</option>
+          <option value="Relaxed">Relaxed</option>
+          <option value="Tense">Tense</option>
+          <option value="Excited">Excited</option>
+          <option value="Distracted">Distracted</option>
+        </SelectStyle>
+        <br></br>
+        <SubmitBitton>Submit</SubmitBitton>
+      </MainDiv>
+    );
+  }
 }
 
+connect(mapStateToProps)(Feeling);
 export default Feeling;
--- client/components/MainHeader.jsx
+++ client/components/MainHeader.jsx
@@ -7,87 +7,91 @@
 \***************************/
 
 import React from 'react';
-import {Component} from 'react';
+import { Component } from 'react';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
 import * as actions from '../../redux/actions.js';
 
 //Styling for the whole header
 const MainDiv = styled.div`
-    width: 100%;
-    border-style: solid;
-    font-family: georgia;
-    text-align: center;
+  width: 100%;
+  border-style: solid;
+  font-family: georgia;
+  text-align: center;
 `;
 
 //Styling for the 'Mood-Ring' title
 const TitleText = styled.p`
-    margin: 2px;
-    font-weight: bold;
-    font-size: 30px;
+  margin: 2px;
+  font-weight: bold;
+  font-size: 30px;
 `;
 
 //Styling for the Log In button
 const LoginButton = styled.button`
-    text-decoration: none;
-    border-radius: 20px;
-    color: black;
-    font-size: 20px;
-    margin: 3px;
-    &:focus{
-        outline: none;
-    }
+  text-decoration: none;
+  border-radius: 20px;
+  color: black;
+  font-size: 20px;
+  margin: 3px;
+  &:focus {
+    outline: none;
+  }
 `;
 
 const mapStateToProps = (reduxState) => {
-    //used to bring in the pieces of state that the components on this page will use
-    return {
-      page: reduxState.page
-    };
+  //used to bring in the pieces of state that the components on this page will use
+  return {
+    page: reduxState.page
   };
-  
-  const mapDispatchToProps = (dispatch) => {
-    //used to bring in actions that will be dispatched within the components on this page.
-    //   syncMarkets: () => dispatch(actions.syncMarkets())
-    return {
-      changePage: (index) => {
-        dispatch(actions.changePage(index));
-      }
-    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  //used to bring in actions that will be dispatched within the components on this page.
+  return {
+    changePage: (index) => {
+      dispatch(actions.changePage(index));
+    }
   };
+};
 
+class MainHeader extends Component {
+  constructor(props) {
+    super(props);
 
-class MainHeader extends Component{
-    constructor(props){
-        super(props);
+    this.gotToLogin = this.gotToLogin.bind(this);
+    this.gotToCreate = this.gotToCreate.bind(this);
+  }
 
-        this.gotToLogin = this.gotToLogin.bind(this);
-        this.gotToCreate = this.gotToCreate.bind(this);
+  gotToLogin() {
+    this.props.changePage(1);
+  }
+
+  gotToCreate() {
+    this.props.changePage(3);
+  }
+
+  render() {
+    console.log('Page in header:', this.props.page);
+    const headerArray = [];
+    headerArray.push(<TitleText>Mood-Ring</TitleText>);
+    if (this.props.page != 'UserFeed') {
+      if (this.props.page != 'Login')
+        headerArray.push(
+          <LoginButton href="" onClick={this.gotToLogin}>
+            Log In
+          </LoginButton>
+        );
+      if (this.props.page != 'Create')
+        headerArray.push(
+          <LoginButton href="" onClick={this.gotToCreate}>
+            Sign Up
+          </LoginButton>
+        );
     }
 
-    gotToLogin(){
-        this.props.changePage(1);
-    }
-
-    gotToCreate(){
-        this.props.changePage(3);
-    }
-
-    render(){
-        console.log("Page in header:", this.props.page);
-        const headerArray = [];
-        headerArray.push(<TitleText>Mood-Ring</TitleText>);
-        if(this.props.page != 'UserFeed'){
-            if(this.props.page != 'Login') headerArray.push(<LoginButton href = "" onClick = {this.gotToLogin}>Log In</LoginButton>);
-            if(this.props.page != 'Create') headerArray.push(<LoginButton href = "" onClick = {this.gotToCreate}>Sign Up</LoginButton>);
-        }
-
-        return(
-            <MainDiv>
-                {headerArray}
-            </MainDiv>
-        )
-    }
+    return <MainDiv>{headerArray}</MainDiv>;
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(MainHeader);
