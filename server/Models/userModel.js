022ef6940a09e09cb82d3ab580c754bf7e97f295
Mitchel Severe <truth531@gmail.com>
2020-01-28 19:11:22 -0500

    Revert "refactor server.js, routes, and models"
    This reverts commit b76249bc5b33d98a313afb2973970c9d6838582c.

--- server/Models/userModel.js
+++
@@ -1,9 +0,0 @@
-const userTable = `
-  CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR NOT NULL,
-    hash VARCHAR NOT NULL
-  );
-`;
-
-module.exports = userTable;
